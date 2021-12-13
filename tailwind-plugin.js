a83ff3ad0d340f8923106f11ebe594d3ebbb5796
Riccardo Caroli <rcaroli@webgriffe.com>
2021-12-13 13:03:23 +0100

    simplified

--- tailwind-plugin.js
+++ tailwind-plugin.js
@@ -3,11 +3,5 @@ const plugin = require('tailwindcss/plugin')
 module.exports = plugin.withOptions(() => {
   return function ({ addVariant, e }) {
 
-    addVariant('on', ({ modifySelectors, separator }) => {
-      modifySelectors(({ className }) => {
-        return `.${e(`on${separator}${className}`)}.on`
-      })
-    })
-
   }
 })
